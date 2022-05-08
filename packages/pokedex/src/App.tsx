a98b0258c4a0dbd8e7e1c2b8a8374e1960776487
christopherkuhtz <christopherkuhtz@gmail.com>
2022-05-08 05:11:36 +0200

    will it publish?

--- packages/pokedex/src/App.tsx
+++ packages/pokedex/src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { helloWorld } from "christopherkuhtz-pokemon-api";
+import { helloWorld } from "chriskuhtz-pokemon-api";
 
 const App = (): JSX.Element => {
   helloWorld();
