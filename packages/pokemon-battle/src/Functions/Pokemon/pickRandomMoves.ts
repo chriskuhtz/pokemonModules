67e20044f4d2ecf42d21152098fdce227e892b03
christopherkuhtz <christopherkuhtz@gmail.com>
2022-06-28 06:53:21 +0200

    pick random moves for each pokemon

---
+++ packages/pokemon-battle/src/Functions/Pokemon/pickRandomMoves.ts
@@ -0,0 +1,25 @@
+// returns the urls of 4 random moves
+
+export const pickRandomMoves = (
+  moves: { move: { url: string } }[]
+): string[] => {
+  console.log(moves);
+  if (moves.length <= 4) {
+    return moves.map((m: { move: { url: string } }) => {
+      return m.move.url;
+    });
+  }
+  const pickedMoves: string[] = [];
+  let i = 0;
+  while (i < 4) {
+    const randomIndex = Math.round(Math.random() * (moves.length - 1));
+    let nextMove = moves[randomIndex];
+    console.log(nextMove, randomIndex);
+    if (!pickedMoves.includes(nextMove.move.url)) {
+      pickedMoves.push(nextMove.move.url);
+      i++;
+    }
+  }
+
+  return pickedMoves;
+};
