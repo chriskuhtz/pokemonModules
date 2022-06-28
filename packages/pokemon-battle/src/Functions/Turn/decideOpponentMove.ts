bd4a70056aa8c713841557030cc0e976be9636b2
christopherkuhtz <christopherkuhtz@gmail.com>
2022-06-28 06:21:55 +0200

    decideOpponentMove

--- packages/pokemon-battle/src/Functions/Turn/decideOpponentMove.ts
+++ packages/pokemon-battle/src/Functions/Turn/decideOpponentMove.ts
@@ -3,7 +3,8 @@ import { PokemonMoveSet } from "../../Models/Pokemon";
 
 export const decideOpponentMove = (moves: PokemonMoveSet): Move => {
   const moveArray = Object.values(moves);
-  const selectedMove = moveArray[Math.round(moveArray.length * Math.random())];
-  console.log(selectedMove.name);
+  const moveIndex = Math.round(moveArray.length * Math.random());
+  const selectedMove = moveArray[moveIndex];
+
   return selectedMove;
 };
