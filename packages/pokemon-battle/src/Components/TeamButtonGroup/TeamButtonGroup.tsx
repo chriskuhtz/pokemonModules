49afc2607e5a45af2bda44fb02ef5791903a757e
christopherkuhtz <christopherkuhtz@gmail.com>
2022-07-05 22:41:12 +0200

    add frontUrl and pokemon icon in TeamBox

--- packages/pokemon-battle/src/Components/TeamButtonGroup/TeamButtonGroup.tsx
+++ packages/pokemon-battle/src/Components/TeamButtonGroup/TeamButtonGroup.tsx
@@ -1,7 +1,15 @@
 import { IconButton, Tooltip, Box } from "@mui/material";
 import SportsBasketballIcon from "@mui/icons-material/SportsBasketball";
 import CatchingPokemonIcon from "@mui/icons-material/CatchingPokemon";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../../Store/store";
+import { addLog } from "../../Store/logSlice";
+
 const TeamButtonGroup = () => {
+  const dispatch = useDispatch();
+  const activePokemon = useSelector(
+    (state: RootState) => state.activePokemon.value
+  );
   return (
     <Box
       height="100%"
@@ -10,10 +18,22 @@ const TeamButtonGroup = () => {
       justifyContent={"space-evenly"}
       borderRight="1px solid darkgray"
     >
-      {[0, 1, 2, 3, 4, 5].map((t) => (
+      {[0, 1, 2, 3, 4, 5].map((t, i) => (
         <Tooltip key={t} disableFocusListener title="Switch" placement="right">
-          <IconButton>
-            <CatchingPokemonIcon fontSize="large" />
+          <IconButton
+            onClick={() => {
+              dispatch(
+                addLog({
+                  message: "Switching and multiple Pokemon coming soon",
+                })
+              );
+            }}
+          >
+            {i === 0 ? (
+              <img height="45px" width="45px" src={activePokemon.frontUrl} />
+            ) : (
+              <CatchingPokemonIcon fontSize="large" />
+            )}
           </IconButton>
         </Tooltip>
       ))}
